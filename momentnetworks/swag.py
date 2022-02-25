2a04c13889afba77e929943a3d23fcb352ed6683
Pablo-Lemos <plemos91@gmail.com>
2022-02-25 09:19:54 +0000

    Added SWA model

--- momentnetworks/swag.py
+++ momentnetworks/swag.py
@@ -1,6 +1,78 @@
 import tensorflow as tf
-import keras
+import numpy as np
 from tensorflow.keras.layers import Dense, Dropout, LeakyReLU, Input, Flatten
+
+
+class SwaModel(tf.keras.Model):
+    def __init__(self):
+        super(SwaModel, self).__init__()
+        self.w_avg = None
+        self.w2_avg = None
+        self.pre_D = None
+        self.n_models = 0
+        self.K = 20
+
+    def flatten(self):
+        flat = tf.zeros(0)
+        for layer in self.weights:
+            flat = tf.concat([flat, tf.reshape(layer, -1)], axis=0)
+        return flat
+
+    def aggregate_model(self):
+        # """Aggregate parameters for SWA/SWAG"""
+        cur_w = self.flatten()
+        cur_w2 = cur_w ** 2
+
+        if self.w_avg is None:
+            self.w_avg = cur_w
+            self.w2_avg = cur_w2
+        else:
+            self.w_avg = (self.w_avg * self.n_models + cur_w) / (self.n_models + 1)
+            self.w2_avg = (self.w2_avg * self.n_models + cur_w2) / (self.n_models + 1)
+
+        if self.pre_D is None:
+            self.pre_D = cur_w.clone()[:, None]
+        else:
+            # Record weights, measure discrepancy with average later
+            self.pre_D = tf.concat((self.pre_D, cur_w[:, None]), axis=1)
+            if self.pre_D.shape[1] > self.K:
+                self.pre_D = self.pre_D[:, 1:]
+
+        self.n_models += 1
+
+    def load_weights(self, arr):
+        currWeights = self.weights
+        currIndex = 0
+        for i, layer in enumerate(currWeights):
+            n = np.prod(layer.shape)
+            w = arr[currIndex:currIndex+n]
+            currIndex+=n
+            self.weights[i] = tf.reshape(w, layer.shape)
+
+    def sample_weights(self, scale=0.5):
+        """Sample weights using SWAG:
+        - w ~ N(avg_w, 1/2 * sigma + D . D^T/2(K-1))
+            - This can be done with the following matrices:
+                - z_1 ~ N(0, I_d); d the number of parameters
+                - z_2 ~ N(0, I_K)
+            - Then, compute:
+            - w = avg_w + (1/sqrt(2)) * sigma^(1/2) . z_1 + D . z_2 / sqrt(2(K-1))
+        """
+        avg_w = self.w_avg  # [K]
+        avg_w2 = self.w2_avg  # [K]
+        D = self.pre_D - avg_w[:, None]  # [d, K]
+        d = avg_w.shape[0]
+        K = self.K
+        z_1 = tf.random.normal((1, d))
+        z_2 = tf.random.normal((K, 1))
+
+        w = avg_w[None] + scale * (1.0 / np.sqrt(2.0)) * z_1 * tf.abs(
+            avg_w2 - avg_w ** 2) ** 0.5
+        w += scale * (D @ z_2).T / np.sqrt(2 * (K - 1))
+        w = w[0]
+
+        self.load_weights(w)
+        return w
 
 
 class SimplerLeaky(tf.keras.Model):
@@ -26,3 +98,34 @@ class SimplerLeaky(tf.keras.Model):
         x = self.dense2(x)
         x = self.act2(x)
         return self.dense3(x) # Return results of Output Layer
+
+
+class SwaLeaky(SwaModel):
+    def __init__(self):
+        # Initialize the necessary components of tf.keras.Model
+        super(SwaLeaky, self).__init__()
+        # Now we initalize the needed layers - order does not matter.
+        # -----------------------------------------------------------
+        # Flatten Layer
+        self.flatten = Flatten()
+        # First Dense Layer
+
+        self.dense1 = Dense(128)
+        self.act1 = LeakyReLU(alpha=0.1)
+        self.dense2 = Dense(128)
+        self.act2 = LeakyReLU(alpha=0.1)
+        self.dense3 = Dense(128)
+
+    def call(self, inputs):
+        x = self.flatten(inputs)
+        x = self.dense1(x)
+        x = self.act1(x)
+        x = self.dense2(x)
+        x = self.act2(x)
+        return self.dense3(x) # Return results of Output Layer
+
+
+if __name__ == "__main__":
+    model = SwaLeaky()
+    model.compile()
+    print(model)
